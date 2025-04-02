d138b786de2173fba9c9546ca6979e2748b0760a
Rodrigo KS <rodrigoks@gmail.com>
2025-04-02 20:29:36 -0300

    feat: criada a entidade Product.

---
+++ src/main/java/br/com/jbusiness/crudapplication/domain/product/Product.java
@@ -0,0 +1,28 @@
+package br.com.jbusiness.crudapplication.domain.product;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Table(name = "product")
+@Entity(name = "product")
+@EqualsAndHashCode(of = "id")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+
+    @Id @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private String name;
+    private Number priceInCents;
+
+}
